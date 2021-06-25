42141244b6d887695b3d0c6668e75ce0a56cf27d
Mark Wardle <mark@wardle.org>
2021-06-25 12:29:04 +0100

    Remove datascript experiment.

--- src/com/eldrix/oink/core.clj
+++ src/com/eldrix/oink/core.clj
@@ -1,68 +1,29 @@
 (ns com.eldrix.oink.core
   (:require [clojure.core.async :as async]
             [com.eldrix.oink.importer :as importer]
-            [com.eldrix.oink.store :as store]
-            [datalevin.core :as d]
-            [datascript.core :as ds]))
+            [datalevin.core :as d]))
 
 (comment
+  (def schema {:LOINC_NUM  {:db/unique :db.unique/identity}})
 
-  (def ds-conn (ds/create-conn))
+  (def st (d/create-conn "/tmp/oink1" schema))
   (def ch (async/chan))
-  (async/go (importer/stream "/Users/mark/Downloads/Loinc_2.70" ch :batch-size 10000))
-  (time (loop [i 0
-               total 0
-               batch (async/<!! ch)
-               futures []]
-          (if-not (seq batch)
-            (dorun (map deref futures))
-            ;(println "Processed " total "; processing batch " i)
-            (recur (inc i)
-                   (+ total (count (:data batch)))
-                   (async/<!! ch)
-                   (conj futures (ds/transact-async ds-conn (:data batch)))))))
-
-  (ds/q '[:find ?e ?long-common-name
-          :in $ ?class
-          :where
-          [?e :CLASS ?class]
-          [?e :LONG_COMMON_NAME ?long-common-name]]
-        @ds-conn
-        "CHEM")
-  (time (ds/q '[:find ?e ?long-common-name
-                :in $ ?loinc-num
-                :where
-                [?e :LOINC_NUM ?loinc-num]
-                [?e :LONG_COMMON_NAME ?long-common-name]]
-              @ds-conn
-              "2951-2"))
-
-  (def st (store/open-store "/tmp/oink1"))
-  (def ch (async/chan))
-  (async/thread (importer/stream "/Users/mark/Downloads/Loinc_2.70" ch :batch-size 5000))
+  (async/thread (importer/stream "/home/mark/Downloads/Loinc_2.70" ch :batch-size 5000))
 
   (time
     (loop [i 0
            total 0
-           batch (async/<!! ch)
-           results []]
-      (if-not (seq batch)
-        (do
-          (println "Import complete. Finishing processing.")
-          (dorun (map deref results))
-          (println "Processing done"))
-        (do
-          (println "Processed " total "; processing batch " i)
-          (recur (inc i)
-                 (+ total (count (:data batch)))
-                 (async/<!! ch)
-                 (conj results (d/transact-async st (:data batch))))))))
+           batch (async/<!! ch)]
+      (when (seq batch)
+        (d/transact! st (:data batch))
+        (recur (inc i)
+               (+ total (count (:data batch)))
+               (async/<!! ch)))))
 
   (def batch (async/<!! ch))
   batch
   (d/transact! st (:data batch))
   (d/schema st)
-  (d/datoms (d/db st) :eav "10448-9")
   (d/db st)
 
   (require '[datalevin.core :as d])
@@ -73,16 +34,28 @@
                [?e :LONG_COMMON_NAME ?long-common-name]]
              (d/db st)
              "2951-2"))
-  (d/q '[:find ?e ?long-common-name
+  (d/q '[:find (count ?e)
          :in $ ?class
          :where
-         [?e :CLASS ?class]
-         [?e :LONG_COMMON_NAME ?long-common-name]]
+         [?e :CLASS ?class]]
        (d/db st)
        "CHEM")
+
+  (d/q '[:find (count ?class)
+         :where
+         [_ :CLASS ?class]]
+       (d/db st))
+
+  (d/q '[:find [?class ...]
+         :where
+         [_ :CLASS ?class]]
+       (d/db st))
+
+
   (d/datoms (d/db st) :eav 21266)
 
-
+  (:DisplayName (d/entity (d/db st) [:LOINC_NUM "2951-2"]))
+  (seq (d/entity (d/db st) [:LOINC_NUM "2951-2"]))
 
   (store/close-store st)
   )
